e2e9c2e256784563adf672b420470463aba5eee3
Kevin Bradwick <kevinbradwick@gmail.com>
2020-12-28 22:01:15 +0000

    wip

--- SwiftUIExamples/Screens/Animations/TransitionScreen.swift
+++ SwiftUIExamples/Screens/Animations/TransitionScreen.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 
+// custom transition to slide up on enter and down on out
+extension AnyTransition {
+    static var popup: AnyTransition {
+        AnyTransition.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom))
+    }
+}
+
 struct TransitionScreen: View {
+    
+    // the overlay that will be presented on the screen
     struct Overlay: View {
         private let gradientFrom = Color(red: 240/255, green: 105/255, blue: 102/255)
         private let gradientTo = Color(red: 250/255, green: 214/255, blue: 166/255)
+        
+        @Binding var showOverlay: Bool
         
         var body: some View {
             ZStack {
@@ -21,7 +32,9 @@ struct TransitionScreen: View {
                             endPoint: .topLeading))
                 VStack {
                     Text("Overlay").font(.title).bold()
-                    Button(action: {}) {
+                    Button(action: {
+                        self.showOverlay.toggle()
+                    }) {
                         HStack {
                             Image(systemName: "xmark")
                             Text("Close")
@@ -36,9 +49,26 @@ struct TransitionScreen: View {
         }
     }
     
+    @State var showOverlay: Bool = false
+    
     var body: some View {
+        // applying zIndex are important here to prevent unwanted layering issues
+        // when the content is being re-rendered.
         ZStack {
-            Overlay()
+            Button(action: {
+                self.showOverlay.toggle()
+            }) {
+                Text("Show Overlay")
+            }
+            .zIndex(1)
+            
+            if showOverlay {
+                Overlay(showOverlay: $showOverlay)
+                    .transition(.popup)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .zIndex(2)
+            }
         }
     }
 }
